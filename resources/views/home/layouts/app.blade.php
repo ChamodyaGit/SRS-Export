cf9a1ee3ec093bab9a14ad5d64366eb484e8c9a6
CyberElysiumDev <backchamodyapunsara@gmail.com>
2024-12-29 02:42:21 +0530

    changes

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="meta description">
+    <meta name="description" content="To deliver the finest quality Ceylon cinnamon to global markets while promoting sustainable and ethical farming practices.">
     <title>SRS Exports Holdings</title>
 
     @include('home.libraries.styles')
